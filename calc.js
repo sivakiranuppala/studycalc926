7ad736a00dba444979a5e48db4ffb45dbbf3a536
sivakiranuppala <127392028+sivakiranuppala@users.noreply.github.com>
2023-03-23 15:18:50 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,16 @@
+function add(x,y){
+    return x+y;
+}
+function sub(x,y){
+    return x-y;
+}
+function mul(x,y){
+    return x*y;
+}
+function div(x,y){
+    return x/y;
+}
+exports.add=add;
+exports.sub =sub;
+exports.mul =mul;
+exports.div =div;
